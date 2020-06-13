88b4bd1b31a77275cc58d063d5c2b1ae9943d4a3
eozuna3 <eo30239@gmail.com>
2020-06-13 13:49:40 -0500

    I created a server.js file.  Moved route folder to the main traccur folder. Created a component for the homepage.

---
+++ src/pages/homepage.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Homepage extends Component {
+     render = () => {
+          return (
+               <div>
+                    <h1 id="title">TR<span id="redTitle">Λ</span>CC<span id="redTitle">V</span>R</h1>
+
+                    <div id="container">
+                         <div id="splashWrap">
+                              <p id="splashTextTitle">WELCOME</p>
+                              <p className="splashText">TRΛCCVR is a browser-based mouse tracking game</p>
+                              <br />
+                              <p className="splashText yellow">THE RULES ARE SIMPLE:</p>
+                              <p className="splashTextRules"><span className="yellow">1</span>] FOLLOW THE TARGET WITH YOUR MOUSE</p>
+                              <p className="splashTextRules"><span className="yellow">2</span>] THAT'S IT</p>
+                              <br />
+                              <p className="splashText">SOUND SIMPLE?</p>
+                              <p className="splashText">TOLD YOU!</p>
+                              <br />
+                              <p className="splashText">BUT BE CAREFUL... IT GETS <span className="red">HARDER</span></p>
+                              <br />
+                         </div>
+                         <div id="animation"></div>
+                         <div id="startBtnWrap">
+                              <div id="startBtn" onclick="game(0)">
+                                   <p id="startTxt">PLΛY</p>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          );
+     }
+}
+
+export default Homepage;
